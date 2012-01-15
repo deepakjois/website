4f992a1817fe5e285c935b86b299b70d5e7a4536
Deepak Jois <deepak.jois@gmail.com>
2012-01-14 18:24:50 -0800

    hakyll layout first cut

--- bin/site.hs
+++ bin/site.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Control.Arrow ((>>>))
+import Control.Arrow (arr, (>>>))
 
 import Hakyll
 
@@ -9,10 +9,34 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
+    match "js/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
+
+    match "images/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
+    match "favicon.ico" $ do
+        route idRoute
+        compile copyFileCompiler
+
+    match "analytics.html" $ compile pageCompiler
+    match "nav.html" $ compile pageCompiler
+
     match "templates/*" $ compile templateCompiler
 
-    match (list ["index.markdown"]) $ do
-                  route   $ setExtension "html"
-                  compile $ pageCompiler
-                       >>> applyTemplateCompiler "templates/default.html"
-                       >>> relativizeUrlsCompiler
+    match "index.markdown" $ do
+        route $ setExtension "html"
+        compile $ pageCompiler
+             >>> requireA "analytics.html" (setFieldA "analytics" $ arr pageBody)
+             >>> requireA "nav.html" (setFieldA "nav" $ arr pageBody)
+             >>> applyTemplateCompiler "templates/home.html"
+
+    match "code.markdown" $ do
+        route $ setExtension "html"
+        compile $ pageCompiler
+             >>> requireA "analytics.html" (setFieldA "analytics" $ arr pageBody)
+             >>> requireA "nav.html" (setFieldA "nav" $ arr pageBody)
+             >>> applyTemplateCompiler "templates/inner.html"
