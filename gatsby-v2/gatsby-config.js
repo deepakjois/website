2d76dd2850bae3c9c43b83b2c7ed8cdd14ccb864
Deepak Jois <deepak.jois@gmail.com>
2018-08-03 12:58:02 +0530

    Adding dependencies and config for posts support

--- gatsby-v2/gatsby-config.js
+++ gatsby-v2/gatsby-config.js
@@ -3,6 +3,29 @@ module.exports = {
     title: 'Deepak Jois – Website',
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-plugin-feed`,
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-glamor`,
     {
